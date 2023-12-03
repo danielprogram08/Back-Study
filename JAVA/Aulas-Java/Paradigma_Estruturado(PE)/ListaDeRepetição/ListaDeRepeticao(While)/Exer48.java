11ae7aea57fa3e93828307946f0c86fd4b5adab4
danielprogram08 <danieldanielsilva08@gmail.com>
2023-12-02 23:41:17 -0300

    Questão 48, alterada (incompleta)!

--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/ListaDeRepetição/ListaDeRepeticao(While)/Exer48.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/ListaDeRepetição/ListaDeRepeticao(While)/Exer48.java
@@ -14,42 +14,35 @@ import java.util.Scanner;
 public class Exer48 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int cr = 0, mes, i = 0, masc = 0, fem = 0;
+        int nascidas = 0, meses, countM = 0, crmasculina = 0, crfeminina = 0, crMortas;
         char sexo, resp = 's';
-        double result, percent = 15.0/100.0;
 
-        //1. && 2.
-        while (resp == 's') {
-            System.out.print("Informe o sexo da criança: [m/f]: ");
-            sexo = in.next().charAt(0);
-            if (sexo == 'm') {
-                masc++;
-            } else {
-                fem++;
-            }     
+        while (resp == 's' || (crmasculina + crfeminina) <= nascidas) {
+            System.out.println("Informe o número de crianças nascidas no período: ");
+                nascidas = in.nextInt();   
+            System.out.println("Informe o sexo de uma criança morta [m/f]: ");
+                sexo = in.next().charAt(0);
+                    if (sexo == 'm') {
+                        crmasculina++;
+                    } else if (sexo == 'f') {
+                        crfeminina++; 
+                    }
 
-            System.out.println("Informe quantos meses tinha a criança: ");
-            mes = in.nextInt();
-            i++;
-          
-        System.out.println("Deseja continuar ? [s/n]: ");
-            resp = in.next().charAt(0);      
-        cr++;   
+            System.out.println("Informe o número de meses de vida da criança: ");
+                meses = in.nextInt();
+                    if (meses <= 24) {
+                        countM++;
+                    }
 
+            System.out.println("Deseja continuar ? [s/n]: ");
+                resp = in.next().charAt(0);
+                    if (resp == 'n') {
+                        break;
+                    }                       
         }
-        
-        //1. && 2.
-        System.out.println("Número de crianças nascidas no período: " + cr);
-        System.out.println("Número de nascidos entre 24 meses: " + i);
-        System.out.println("Número de crianças do sexo masculino: " + masc);
-        System.out.println("Número de crianças do sexo feminino: " + fem);
-        //A)
-        System.out.println("Porcentagem de crianças mortas no período: " + (result = cr/percent));
-        //B)
-        System.out.println("Porcentagem de crianças mortas do sexo masculino no período: " + (result = masc/percent));
-        //C)
-        System.out.println("Porcentagem de crianças que viveram no período de 24 meses ou menos: " + (result = i/percent));
-        
+
+        System.out.println("O número de crianças mortas no período: " + (crMortas = crmasculina + crfeminina));
+        System.out.println("O número de crianças do sexo masculino mortas no período: ");
+        System.out.println("O número de crianças que viveram 24 meses ou menos no período: ");
     }
-    
 }
