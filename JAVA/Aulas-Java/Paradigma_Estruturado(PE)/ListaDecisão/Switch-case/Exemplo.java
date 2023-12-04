335358cca9408741b5c09cb9a644ce6376d0d10b
danielprogram08 <danieldanielsilva08@gmail.com>
2023-12-04 19:50:17 -0300

    Adicionado Switch-Case!

---
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/ListaDecisão/Switch-case/Exemplo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Exemplo {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        //Dias da semana usando If & Else;
+        int DiaSemana = 0;
+        
+        if (DiaSemana == 1) {
+            System.out.println("Domingo");
+        }
+
+    }
+}
