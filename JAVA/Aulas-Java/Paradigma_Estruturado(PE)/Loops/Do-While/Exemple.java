394d37947fe2a6288d0d6fc7cadc0641337f626e
Daniel <115734771+danielprogram08@users.noreply.github.com>
2023-12-19 19:19:33 -0300

    Update Exemple.java

--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/Do-While/Exemple.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/Do-While/Exemple.java
@@ -4,8 +4,10 @@ public class Exemple {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+//iniciando variável com 1;
 int i = 1;
 
+//Mostrando na tela os valores incrementando mais 1 e rodando enquanto for menor que ou igual a 10;
 do {
 System.out.println(i);  
   i++; 
