d5150d3c3edecca8cc69cada499601d9d6994222
Daniel <115734771+danielprogram08@users.noreply.github.com>
2023-12-19 19:15:18 -0300

    Alteração no arquivo de exemplo com Do-While!

--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/Do-While/Exemple.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/Do-While/Exemple.java
@@ -3,6 +3,13 @@ import java.util.Scanner;
 public class Exemple {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        
+
+int i = 1;
+
+do {
+System.out.println(i);  
+  i++; 
+} while(i <= 10);  
+      
     }
 }
