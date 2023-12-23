6f4fef5f0177753bc0594a43b703d26d83fe0606
danielprogram08 <danieldanielsilva08@gmail.com>
2023-12-23 18:21:21 -0300

    Alterações no arquivo de exemplo!

--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/Do-While/Exemple.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/Do-While/Exemple.java
@@ -4,24 +4,24 @@ public class Exemple {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int i = 1;
+        int count = 1;
         int max = 10;
 
         System.out.println("Contando até: " + max);
 
         //While;
-        while (i <= max) {
-            System.out.println("valor de i: " + i);
-            i++; //i + 1 ou i += 1; 
+        while (count <= max) {
+            System.out.println("valor de i: " + count);
+            count++; //i + 1 ou i += 1; 
         }
-        System.out.println(i); //valor de 11;
+        System.out.println(count); //valor de 11;
 
         //Do-While;
         do {
-            i++;
+            count++;
             System.out.println("valor de i: " + i);
-        } while (i < 15); 
+        } while (count < 15); 
 
-        System.out.println(i);
+        System.out.println(count);
     }
 }
