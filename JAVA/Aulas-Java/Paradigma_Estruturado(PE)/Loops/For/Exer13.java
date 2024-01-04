5364b4ca1f98cc5a88f0a5a9c02bd45026a33252
danielprogram08 <danieldanielsilva08@gmail.com>
2024-01-04 12:39:37 -0300

    Update in Exercice 13!

--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/For/Exer13.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Loops/For/Exer13.java
@@ -5,7 +5,7 @@ public class Exer13 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        int valor, raiz, mul;
+        int valor, raiz;
 
         for (int i = 1; i <= 15; i++) {
             System.out.print("Insira uma raiz: ");
