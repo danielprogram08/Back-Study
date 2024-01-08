1113b979dd9db3fe6c7960cfde63652fb6328194
danielprogram08 <danieldanielsilva08@gmail.com>
2024-01-08 19:05:31 -0300

    Alteração refeita nos exer de Array (Incompleto)!

--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Vetores/Arrays/Exer16.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Vetores/Arrays/Exer16.java
@@ -1,15 +1,29 @@
 package Vetores.Arrays;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Exer16 {
     public static void main(String[] args) {
         Random valores = new Random();
-        int valor,soma;
+        Scanner in = new Scanner(System.in);
+        int[] A = new int[10];
+        int[] position = new int[2];
+        int valorA,valor,soma,media;
+        int mquinze = 0, i = 0, iquinze = 0, Mquinze = 0;
 
-        valor = valores.nextInt(10);
-        int[] A = {valor, valor, valor, valor, valor, valor, valor, valor, valor, valor};
+        System.out.print("insira um valor limite: ");
+            valor = in.nextInt();
+            valorA = valores.nextInt(valor);
 
-        soma = ((A[0] + A[1]) + (A[2] + A[3]) + (A[4] + A[5]));
+        for (int count = 0; count < A.length; count++) {
+            A[count] = valores.nextInt(valorA);
+            
+        }
+
+        //System.out.println("Soma de elementos armazenados neste vetor que são inferiores a 15: " + mquinze);
+        //System.out.println("Quantidade de elementos que são iguais a quinze: " + iquinze);
+        //System.out.println("A média dos elementos armazenados no vetor que são superiores a 15: " + Mquinze);
+        
     }
 }
--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Vetores/Arrays/Exer17.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Vetores/Arrays/Exer17.java
@@ -6,14 +6,17 @@ public class Exer17 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
             int idade, tf = 0;
-        
+            int[] A = new int[3];
+
             for (int p = 1; p <= 3; p++) {
                 System.out.println("insira a sua idade: ");
                     idade = in.nextInt();
                         if (idade > 35) {
                             tf++;
                         }
-                int[] A = {p, p, p};
+                for (int i = 0; i < A.length; i++) {
+                    A[i] = idade;
+                }
             }
 
             System.out.println("O número de pessoas que são maiores de 35 são: " + tf);
