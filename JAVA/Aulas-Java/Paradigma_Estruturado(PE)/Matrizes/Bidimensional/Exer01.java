fcb7e62094add435d0d2c1350c6dfb39e750c44b
danielprogram08 <danieldanielsilva08@gmail.com>
2024-01-17 00:13:10 -0300

    Exercício 1 de Matriz concluído!

--- JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Matrizes/Bidimensional/Exer01.java
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Matrizes/Bidimensional/Exer01.java
@@ -12,24 +12,26 @@ public class Exer01 {
         int FourV1 = valores.nextInt(9), FourV2 = valores.nextInt(9), FourV3 = valores.nextInt(9), FourV4 = valores.nextInt(9); 
 
         int[][] M = {{v1,v2,v3,v4}, {twoV1,twoV2,twoV3,twoV4}, {threeV1,threeV2,threeV3,threeV4}, {FourV1,FourV2,FourV3,FourV4}};
-        
-        for (int i = 0; i < M.length; i++) {
-            int posicao;
-            posicao = i;
-            System.out.println();
+        int Mvalor = M[0][0];
+        int linhaMvalor = 0;
+        int colunaMvalor = 0;
+        int valor = 0;
 
+        for (int i = 0; i < M.length; i++) {
             for (int j = 0; j < M[i].length; j++) {
-                int FirstPosition = 0;
-                int valor, valorR = 0; 
+
+                //valores do Array;
                 valor = M[i][j];
 
-                //System.out.println("posição: " + posicao + " valor: " + valor);
-
-                if (posicao == 0) {
-                    System.out.println("Valor: " + valor);
-                }
-                
+                //Algorítmo para descobrir o maior valor, sua coluna e linha;
+                if (valor > Mvalor) {
+                    Mvalor = valor;
+                    linhaMvalor = i;
+                    colunaMvalor = j;
+                }    
             }
         }
+
+        System.out.println("Maior valor: " + Mvalor + " linha: " + linhaMvalor + " coluna: " + colunaMvalor);
     }
 }
