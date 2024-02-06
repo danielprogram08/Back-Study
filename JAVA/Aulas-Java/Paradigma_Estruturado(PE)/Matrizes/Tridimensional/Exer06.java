6713c3bd320dc1eb806323037f6e01f32bb0b808
danielprogram08 <danieldanielsilva08@gmail.com>
2024-02-05 22:50:06 -0300

    Started Exer06!

---
+++ JAVA/Aulas-Java/Paradigma_Estruturado(PE)/Matrizes/Tridimensional/Exer06.java
@@ -0,0 +1,11 @@
+package Matrizes.Tridimensional;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exer06 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random maquina = new Random();
+    }
+}
