bcd798ba6b592adea30de9da97b6cda225b81f50
danielprogram08 <danieldanielsilva08@gmail.com>
2024-02-13 15:39:17 -0300

    Alterações no arquivo!

--- JAVA/Aulas/POO/Classes/Exemple.java
+++
@@ -1,7 +0,0 @@
-package JAVA.Aulas.POO.Classes;
-
-public class Exemple {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
---
+++ JAVA/Aulas/POO/Classes/carro.java
@@ -0,0 +1,10 @@
+package JAVA.Aulas.POO.Classes;
+
+//Objeto;
+
+public class carro {   
+    String marca;
+    String modelo;
+    int npassageiro;
+    double combustivel, consumo;
+}
