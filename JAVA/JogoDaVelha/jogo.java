e5a4db79901e281f0466bfb2eb95424e2462197d
danielprogram08 <danieldanielsilva08@gmail.com>
2024-03-19 23:33:09 -0300

    90% completo!

--- JAVA/JogoDaVelha/jogo.java
+++ JAVA/JogoDaVelha/jogo.java
@@ -7,87 +7,96 @@ public class jogo {
         Scanner in = new Scanner(System.in);
 
         //variáveis do jogo;
-        char[][] tabuleiro = new char[1][3];
+        char[][] tabuleiro = new char[3][3];
+        char sinal, sinal2;
         String player1, player2;
-        char sinal1, sinal2;
-        int jogadas, col, col2, lin1, lin2;
         boolean ganhou = false;
+        int jogada = 1;
+        int linha, coluna, linha2, coluna2;
         
-        //tabuleiro
-        for (int i = 0; i < tabuleiro.length; i++) {
-            System.out.println();
-
-            for (int j = 0; j < tabuleiro[i].length; j++) {
-                System.out.println(i + " | " + j + " | " + '9');
-                System.out.println("--"+"--");
-            }
-        }
-
-        //autenticando os players;
-        System.out.println("Identifique-se Player 1: ");
+        //autenticando players;
+        System.out.println("player 1, informe seu nome: ");
             player1 = in.nextLine();
-        System.out.println("Identifique-se Player 2: ");
+                System.out.println();    
+        
+        System.out.println("player 2, informe seu nome: ");
             player2 = in.nextLine();
-        System.out.println(player1 + " e " + player2 + ". Bem-Vindo ao jogo da velha!");
+                System.out.println();
+        System.out.println(player1 + " e " + player2 + ", bem-vindo ao jogo da velha!");
 
-        //validando os sinais;
-        System.out.println(player1 + " escolha um sinal entre X e O: ");
-            sinal1 = in.next().charAt(0);
+        //escolha dos sinais;
+        System.out.println(player1 + " escolha um sinal entre 'x' e 'o': ");
+            sinal = in.next().charAt(0);
+            if (sinal == 'x' || sinal == 'X') {
+                sinal2 = 'o';
+                System.out.println(player2 + " jogará com: " + "'" + sinal2 + "'.");
+            } else if (sinal == 'o' || sinal == 'O') {
+                sinal2 = 'x';
+                System.out.println(player2 + " jogará com: " + "'" + sinal2 + "'.");
+            } else {
+                System.out.println("sinal inválido. Tente novamente!");
+                    return;
+            }
 
-            do {
-                if (sinal1 == 'o' || sinal1 == 'O') {
-                    sinal2 = 'x';
-                    System.out.println(player2 + " jogará com o " + sinal2);
-                } else if (sinal1 == 'x' || sinal1 == 'X') {
-                    sinal2 = 'o';
-                    System.out.println(player2 + " jogará com o " + sinal2);
-                } else {
-                    System.out.println("Sinal inválido. Tente novamente!");
-                }
-            } while (sinal1 != 'x' && sinal1 != 'o');
-
-        //iniciando o jogo;
+        //iniciando o jogo da velha;
         while (!ganhou) {
-            jogadas = 1;   
-            
+
             //player 1;
-            if (jogadas % 2 == 1) {
-                System.out.println(player1 + " escolha uma coluna [1 - 3]: ");
-                    col = in.nextInt();
-                    if (col < 1 && col > 3) {
-                        System.out.println("insira um valor válido de 1 a 3!");
-                            continue;
+            if (jogada % 2 == 1) {
+                System.out.println();
+                System.out.println("Será a vez do " + player1 + ", escolha a posição da linha [1 - 3]: ");
+                linha = in.nextInt();
+                linha--;
+                    if (linha < 0 || linha > 2) {
+                        System.out.println("posição inválida. Tente novamente!");
+                            return;
                     }
+                System.out.println(player1 + " escolha a posição da coluna [1 - 3]: ");
+                coluna = in.nextInt();
+                    System.out.println();
+                coluna--;
+                    if (coluna < 0 || coluna > 2) {
+                        System.out.println("posição inválida. Tente novamente!");
+                            return;
+                    }
+                tabuleiro[linha][coluna] = sinal;
+
+            //player 2;
+            } else if (jogada % 2 == 0) { 
+                System.out.println();       
+                System.out.println("Será a vez do " + player2 + ", escolha a posição da linha [1 - 3]: ");
+                linha2 = in.nextInt();
+                linha2--;
+                    if (linha2 < 0 || linha2 > 2) {
+                        System.out.println("posição inválida. Tente novamente!");
+                            return;
+                    }
+                System.out.println(player2 + " escolha a posição da coluna [1 - 3]: ");
+                coluna2 = in.nextInt();
+                    System.out.println();
+                coluna2--;
+                    if (coluna2 < 0 || coluna2 > 2) {
+                        System.out.println("posição inválida. Tente novamente!");
+                            return;
+                    }
+                tabuleiro[linha2][coluna2] = sinal2; 
+            }
+
+            //tabuleiro;
+            for (int i = 0; i < tabuleiro.length; i++) {
+                for (int j = 0; j < tabuleiro[i].length; j++) {
+                    System.out.print(tabuleiro[i][j]);
+                    if (j < tabuleiro[i].length - 1) {
+                        System.out.print(" | ");
+                    }
+                }
+                System.out.println();
+                if (i < tabuleiro.length - 1) {
+                    System.out.println("---" + "---");
+                }
+            }
                 
-                System.out.println(player1 + " escolha uma linha [1 - 3]: ");
-                    lin1 = in.nextInt();
-                    if (lin1 < 1 && lin1 > 3) {
-                        System.out.println("insira um valor válido de 1 a 3!");
-                            continue;
-                    }
-            } 
-
-                /*player 2;
-                } else if (jogadas % 2 == 0) {
-                    for (int k = 0; k < tabuleiro.length; k++) {
-                        System.out.println();
-
-                        for (int k2 = 0; k2 < tabuleiro[k].length; k2++) {
-                            System.out.println(player2 + " escolha uma coluna [1 - 3]: ");
-                                col2 = in.nextInt();
-                                    if (col2 < 1 && col2 > 3) {
-                                        System.out.println("insira um valor válido de 1 a 3!");
-                                            continue;
-                                    }
-                            System.out.println(player2 + " escolha uma linha [1 - 3]: ");
-                                lin2 = in.nextInt();
-                                    if (lin2 < 1 && lin2 > 3) {
-                                        System.out.println("insira um valor válido de 1 a 3!");
-                                            continue;
-                                    }       
-                        }
-                    }*/         
-        }       
+            jogada++;    
+        }
     }
-    
 }
