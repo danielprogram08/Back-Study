af7de6b0b6a9e1a74289ff39b5aeb513b6ec7359
danielprogram08 <danieldanielsilva08@gmail.com>
2024-09-28 00:49:58 -0300

    Update JavaScript s miniProject.

--- JavaScript/Mini-Projetos/FizzBuzz.js
+++ JavaScript/Mini-Projetos/FizzBuzz.js
@@ -1,3 +1,9 @@
-for (let i = 0; i < 100; i++) {
-    console.log(i+1);
+for (let index = 1; index <= 100; index++) {   
+    if (index % 3 == 0) {
+        console.log("fizz" + "\n");
+    } else if (index % 5 == 0) {
+        console.log("buzz" + "\n");
+    } else {
+        console.log(index + "\n");
+    }
 }
