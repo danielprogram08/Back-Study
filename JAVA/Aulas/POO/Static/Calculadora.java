20a783960a3f46fe555de03a656c86b7696dc83e
danielprogram08 <danieldanielsilva08@gmail.com>
2024-11-26 10:38:07 -0300

    Add Static.

---
+++ JAVA/Aulas/POO/Static/Calculadora.java
@@ -0,0 +1,9 @@
+public class Calculadora {
+    
+    public static int soma (int num1, int num2) {
+        int calc = num1 + num2;
+        return calc;
+    }
+}
+
+//O Static é usado para que a classe não precise ser instanciada.
---
+++ JAVA/Aulas/POO/Static/Calculadora_Test.java
@@ -0,0 +1,7 @@
+public class Calculadora_Test {
+    public static void main(String[] args) {
+        
+        int resultSoma;
+        resultSoma = Calculadora.soma(1, 2);
+    }
+}
