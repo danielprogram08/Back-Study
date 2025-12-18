8a5aa535dc1ac327e88c4e2a09c0554bf9001329
danielprogram08 <danieldanielsilva08@gmail.com>
2025-12-18 12:29:57 -0300

    Add: JavaFX Study

--- JAVA/JAVA-DESKTOP/JavaDesktop/src/App.java
+++ JAVA/JAVA-DESKTOP/JavaDesktop/src/App.java
@@ -1,14 +1,23 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        launch(args);
     }
 
     @Override
     public void start(Stage arg0) throws Exception {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'start'");
+        arg0.setTitle("Academia dos devs!");
+        Button btn = new Button("Clique aqui!");
+        btn.setOnAction(event -> System.out.println("Clicou!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().addAll(btn);
+        arg0.setScene(new Scene(root, 300, 300));
+        arg0.show();
     }
 }
